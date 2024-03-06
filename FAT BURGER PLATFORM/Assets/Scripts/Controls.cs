8e288f63712eb76e94ff27f79c33b3f71db8969c
teejaytee2006 <tjtappenden@gmail.com>
2024-03-06 13:11:13 +0000

    controls (not working) gl have fun

--- FAT BURGER PLATFORM/Assets/Scripts/Controls.cs
+++ FAT BURGER PLATFORM/Assets/Scripts/Controls.cs
@@ -1,18 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Controls : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public float movementSpeed = 3f;
+    public float jumpSpeed = 500f;
+    private bool isGrounded = true;
+    private Rigidbody2D rb;
+ 
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
+ 
     void Update()
     {
-        
+        if (isGrounded)
+        {using UnityEngine;
+
+public class SpriteMovement : MonoBehaviour
+{
+    public float movementSpeed = 3f; // Speed at which the sprite moves
+
+    void Update()
+    {
+        // Move sprite left if A key or left arrow key is pressed
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+
+        // Move sprite right if D key or right arrow key is pressed
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
     }
 }
+
+            rb.gravityScale = 2.1f;
+        }
+ 
+        if (Input.GetKey("a"))
+        {
+            transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey("s"))
+        {
+            if (!isGrounded)
+            {
+                rb.gravityScale += 2;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                rb.AddForce(Vector3.up * jumpSpeed);
+                isGrounded = false;
+            }
+        }
+    }
+ 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
